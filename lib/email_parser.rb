495edc5b24436b2eee239e8ce9d698ca275e3911
Tammy Lam <lamtammy1515@gmail.com>
2020-02-13 03:00:02 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,7 @@ class EmailAddressParser
   attr_accessor :email_addresses
  @@all 
  
- def parse(email_addresses)
-   email_addresses
+ def initialize(email_addresses)
+   @email_addresses = email_addresses
  end 
  end 
